9ae224c58fe5a3af6f1c2bee730cb9e4e87f4f90
Charles Bayley <cbayley95@gmail.com>
2017-08-08 13:44:36 -0500

    Products of my experimenting here

--- problems/multiset.lisp
+++ problems/multiset.lisp
@@ -8,16 +8,24 @@
 (in-package :multiset)
 
 (defclass multiset ()
-  ((internal-set :reader internal-set
-                 :initarg :set
-                 :initform nil)
-   (initial-set)))
+  ((internal-set :initarg :set
+                 :initform nil
+                 :accessor internal-set)
+   (sorter :initarg :sorter
+           :initform '<
+           :accessor sorter)))
+
+(defun list-to-multiset (tlist)
+  (let ((ulist (remove-duplicates tlist)))
+    ulist))
 
 ;; TODO: Add the functionality to convert input lists
 ;;       into multiset internal form lists.
 ;;       Ex: '(1 1 2 3) => '((1 2) (2 1) (3 1))
 ;;       Ex: '(a b c b c a a a) => '((a 4) (b 2) (c 2))
-(defmethod initialize-instance :after ((set multiset) &key))
+(defmethod initialize-instance :after ((set multiset) &key)
+  (when (eql (internal-set set) nil)
+        (setf (internal-set set) (list-to-multiset (internal-set set)))))
 
 (defgeneric intersection (left-set right-set))
 
@@ -27,4 +35,5 @@
 
 (defmethod intersection ((left-set list) (right-set list)))
 
-(setf ms (make-instance 'multiset)) ;temporary test variable
+(setf ms (make-instance 'multiset
+                        :set '(1 2 2 3 3))) ;temporary test variable
